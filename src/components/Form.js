093cf6be759a9a6753304ed691bdf3221802af4c
Keila Lopez <keilalofra@gmail.com>
2022-11-15 13:15:37 -0600

    added button and breaks

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,20 @@
 function Form () {
+    
     return (
       <div>
         <h3>New Book</h3>
         <form>
             <input placeholder="Title"></input>
+            <br></br>
             <input placeholder="Author"></input>
+            <br></br>
             <input placeholder="Image"></input>
+            <br></br>
             <input placeholder="Rating" type="number"></input>
+            <br></br>
             <input placeholder="Description"></input>
+            <br></br>
+            <button>Add New Book</button>
         </form>
       </div>
     )
